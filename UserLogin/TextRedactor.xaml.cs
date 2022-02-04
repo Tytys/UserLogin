90b251130014eca3d525f68479dd1203a33e6053
Саша Прилуцкий <ififirfifir@gmail.com>
2022-02-04 22:48:03 +0300

    Новые приколы

---
+++ UserLogin/TextRedactor.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UserLogin
+{
+    /// <summary>
+    /// Логика взаимодействия для TextRedactor.xaml
+    /// </summary>
+    public partial class TextRedactor : Window
+    {
+        public TextRedactor()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonRead_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ButtonWrite_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void CalculatorOpen_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
